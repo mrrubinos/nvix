0ce852a5ce6ccbae3fd31b126eb93b6a671a17fe
Miguel Rubinos Rodríguez <mrrubinos@gmail.com>
2024-11-20 21:44:33 +0100

    taking the config up to date.

--- config/base/cmp.nix
+++ config/base/cmp.nix
@@ -16,6 +16,7 @@
       enable = true;
       autoEnableSources = true;
       settings = {
+        completion.completeopt = "menu,menuone,noinsert";
         snippet.expand = # lua
           ''
             function(args)
@@ -40,43 +41,32 @@
         mapping = (helpers.mkRaw # lua
           ''
             cmp.mapping.preset.insert({
-              ["<C-k>"] = cmp.mapping.select_prev_item(),
-              ["<C-j>"] = cmp.mapping.select_next_item(),
+              ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+              ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }), 
 
               ["<c-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
               ["<c-d>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 
               ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
 
-              ["<c-CR>"] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Insert,
-                select = true,
-              }),
+              ["<C-cr>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false },
 
               -- Enter to accept the completion or fallback
-              ["<CR>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.confirm({ select = true }) -- Accept currently selected completion
-                else
-                  fallback() -- Default Enter behavior when no completion menu is visible
-                end
-              end, { "i", "s" }),
-
-              ["<c-h>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                elseif require("luasnip").jumpable(-1) then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+              ["<C-l>"] = cmp.mapping(function(fallback)
+                local luasnip = require("luasnip")
+                if luasnip.expand_or_locally_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif luasnip.jumpable(1) then
+                  luasnip.jump(1) 
                 else
                   fallback()
                 end
               end, { "i", "s" }),
 
-              ["<c-l>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif require("luasnip").expand_or_jumpable() then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+              ["<C-h>"] = cmp.mapping(function(fallback)
+                local luasnip = require("luasnip")
+                if luasnip.jumpable(-1) then
+                  luasnip.jump(-1) 
                 else
                   fallback()
                 end
--- config/base/noice.nix
+++ config/base/noice.nix
@@ -7,25 +7,26 @@ in {
       backgroundColour = "#000000";
       render = "minimal";
       stages = "fade_in_slide_out";
+      topDown = false;
     };
     noice = {
       enable = true;
-      lsp = {
-        override = {
-          "vim.lsp.util.convert_input_to_markdown_lines" = true;
-          "vim.lsp.util.stylize_markdown" = true;
-          "cmp.entry.get_documentation" = true;
+      settings = {
+        lsp = {
+          override = {
+            "vim.lsp.util.convert_input_to_markdown_lines" = true;
+            "vim.lsp.util.stylize_markdown" = true;
+            "cmp.entry.get_documentation" = true;
+          };
+          hover.enabled = false;
+          message.enabled = false;
+          signature.enabled = false;
+          progress.enabled = false;
         };
-        hover.enabled = false;
-        message.enabled = false;
-        signature.enabled = false;
-        progress.enabled = false;
       };
     };
   };
   keymaps = [
-    (mkKeymap "n" "<leader>un"
-      (helpers.mkRaw "function () require('notify').dismiss() end")
-      "Dismiss notification")
+    (mkKeymap "n" "<leader>un" ":lua require('notify').dismiss()<cr>" "Dismiss notification")
   ];
 }
